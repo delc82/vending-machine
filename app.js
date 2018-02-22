813b6703985c3b94cb34154a7c38b8474b98f8e2
David Leon <delc82@gmail.com>
2018-02-22 21:24:33 +0000

    Coded app from scratch

--- app.js
+++ app.js
@@ -1,62 +1,50 @@
-var inventoryController = (function(){
-	let inventory = {
-  	products: [25, 50, 75, 100],
-    bills: [10, 20, 50, 100]
-  };
-	return {
-  	getInventory: function() {
-    	return {
-      	drinks: inventory.products,
-        bills: inventory.bills
-      }
+let dataController = (function() {
+    const data = {
+      bills: [5, 10, 20, 50 ,100],
+      products: [
+        {name: 'Dulce candy', price: 25},
+        {name: 'Springles', price: 30},
+        {name: 'Tostadas Grills', price: 40}
+      ]
+    };
+    
+    return {
+        getData: function() {
+            return data;
+        }
     }
-  }
 }());
 
-var controller = (function(inventoryCtrl) {
-	let data = {
-  	drinks: inventoryCtrl.getInventory().drinks,
-    bills: inventoryCtrl.getInventory().bills,
-    message: '',
-  };
-  
-	let funds = function(int, obj) {
-  	data.drinks.map(function(obj) {
-    	if (int < data.drinks[data.drinks.indexOf(obj)]) {
-      	data.message = 'Insufficient funds!';
-      } else {
-      	data.message = 'Thanks for your purchase!';
-      }
-      document.querySelector('.message').textContent = data.message;
-    })
-  };
-  
-  let getChange = function(int, obj) {
-  	let change = 0;
-    change = int - data.drinks[data.drinks.indexOf(obj)];
-    if (change < 0) {
-    	change = 0;
+let UIController = (function() {
+    const DOMstrings = {
+        inputMoney: '#addMoney',
+        inputProdSelect: '#productSelection'
+    };
+    
+    return {
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
     }
-    document.querySelector('.change').textContent = change;
-  };
-  
-	return {
-  	showData: function() {
-    	document.querySelector('.products').textContent = data.drinks;
-      document.querySelector('.bills').textContent = data.bills;
-    },
-  	init: function(int, obj) {
-    	funds(int);
-      getChange(int, obj);
+    
+}());
+
+let controller = (function(dataCtrl, UICtrl) {
+    let data = dataCtrl.getData();
+    let DOM = UICtrl.getDOMstrings();
+    
+    let productSelection = function() {
+        data.products.forEach(function(element) {
+            let newOption = '<option value="' + element.name + '">'+ element.name + '</option>';
+            document.querySelector(DOM.inputProdSelect).insertAdjacentHTML('beforeend', newOption);
+        });
+    };
+    
+    return {
+        init: function() {
+            productSelection();
+        }
     }
-  }
-}(inventoryController));
+}(dataController, UIController));
 
-controller.showData();
-
-(function(){
-	document.querySelector('#selectButton').addEventListener('click', function(e) {
-    controller.init(parseInt(document.querySelector('#addMoney').value), parseInt(document.querySelector('#productSelection').value));
-    e.preventDefault();
-  });
-}())
+controller.init();
